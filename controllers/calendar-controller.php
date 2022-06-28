8fa5311347e1efa8b563a073ee9ae7905c4a8c5b
Alcpt <alcpt@outlook.com>
2022-06-28 11:59:15 +0200

    adding css

--- controllers/calendar-controller.php
+++ controllers/calendar-controller.php
@@ -1,4 +1,18 @@
 <?php
 
+$days = [
+    1 => 'Lundi',
+    2 => 'Mardi',
+    3 => 'Mercredi',
+    4 => 'Jeudi',
+    5 => 'Vendredi',
+    6 => 'Samedi',
+    7 => 'Dimanche'
+];
 
-echo 'coucou';
+
+$year = date('Y');
+$monthNumber = date ('n');
+$monthLetters = date ('F');
+
+echo $monthNumber, $monthLetters;
--- index.php
+++ index.php
@@ -1,21 +1,41 @@
-<?php require_once "controllers/calendar-controller.php"?>
+<?php require_once "controllers/calendar-controller.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lhp8Calendar</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<link rel="stylesheet" href="public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
+
 <body>
- 
+
+    <h1 class="text-center">Calendrier LHP8</h1>
+    <h2><?= $year ?></h2>
+    <h2><?= $monthLetters ?></h2>
+
+    <div class="row justify-content-center p-0 mt-5 mx-0">
+        <div class="col-10 calendar p-0 m-0">
+
+            <!-- Nous réalisons une boucle pour afficher les jours de la semaine -->
+            <?php
+            foreach ($days as $key => $value) { ?>
+                <div class="text-center text-light bg-dark"><?= $value ?></div>
+            <?php } ?>
 
 
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+
+
+        </div>
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
+
 </html>
